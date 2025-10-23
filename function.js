38875b4ec41213cf01d563081395f17dc9294bfe
Gem <mojeedibrahim13@gmail.com>
2025-10-22 17:09:36 -0700

    adding file codes

--- function.js
+++ function.js
@@ -1,3 +1,25 @@
 function sayHello() {
-    
+    console.log("Hello World!");
 }
+
+sayHello();
+
+function sayGreetings(name) {
+    console.log("==================");
+    console.log("Good morning " + name + "!");
+    console.log("==================");
+}
+
+sayGreetings("Gem");
+sayGreetings("Ibrahim");
+sayGreetings("Kolwole");
+
+// return keyword function to return something in a function
+function calculateTax(amount) {
+    let result = amount * 0.0825;
+    
+    return result;
+}
+
+let tax = calculateTax(100);
+console.log(tax);
