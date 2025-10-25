bc17147aa9bc44ddb9f9d017b4f1ecc2e618cc57
Gem <mojeedibrahim13@gmail.com>
2025-10-25 16:06:33 -0700

    arrow file added

---
+++ arrow.js
@@ -0,0 +1,14 @@
+let hi = () => {
+    console.log("How are you");
+}
+hi();
+
+let greeting = (name) => {
+    console.log(`How are you ${name}`);
+}
+greeting("Gem");
+
+let addNo = (a, b) => {
+    return a+b;
+}
+console.log(addNo(45, 6));
