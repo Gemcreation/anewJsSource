fa7594c9f99686e7455ba2db008a7a7ffc1bf2ef
Gem <mojeedibrahim13@gmail.com>
2025-10-25 16:15:06 -0700

    date method added

--- classes.js
+++ classes.js
@@ -7,3 +7,9 @@ class Car{
 }
 let newCar = new Car("Hondard", "4503", 2024);
 console.log(newCar);
+
+class SportCar extends Car{
+    
+}
+let mySportCar = new SportCar("J-wagon", 233432, 2023);
+console.log(mySportCar);
---
+++ date.js
@@ -0,0 +1,8 @@
+let todayDate = new Date();
+console.log(todayDate);
+console.log(todayDate.getHours);
+console.log(todayDate.getTime);
+
+let myBirth = new Date("August, 2002 07:05:22");
+let elapseTime = todayDate - myBirth;
+console.log(elapseTime);
