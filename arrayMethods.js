fc06dbcef7d15a1fd9e28f4b7ad2d4a4c4922921
Gem <mojeedibrahim13@gmail.com>
2025-10-25 17:32:58 -0700

    commiting changes

---
+++ arrayMethods.js
@@ -0,0 +1,8 @@
+let names = ["Gem", "Kola", "Ayo", "Sola"];
+let others = ["Sammy", "Cary", "Kunle", "Fola"];
+let lost = [4, 8, 15, 16, 23, 42];
+let fibonacci = [1, 1, 2, 3, 5, 8, 13, 23, 34, 35];
+
+let combine = fibonacci.concat(lost);
+console.log(combine);
+console.log(combine.join("-"));
